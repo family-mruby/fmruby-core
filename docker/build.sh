d53c8f2861757530339dbde3c124d22781bdfa6f
kishima <kishima7@gmail.com>
2026-05-07 00:01:03 +0900

    chore: bump ESP-IDF docker image to v5.5.4
    - .env / Rakefile / docker/build.sh fallback: v5.5.1 -> v5.5.4
    - docker/Dockerfile base: release-v5.4 -> release-v5.5

--- docker/build.sh
+++ docker/build.sh
@@ -6,7 +6,7 @@ if [ -f "$SCRIPT_DIR/../.env" ]; then
     source "$SCRIPT_DIR/../.env"
 fi
 
-IDF_VER=${ESP_IDF_VERSION:-v5.5.1}
+IDF_VER=${ESP_IDF_VERSION:-v5.5.4}
 
 docker build \
   --build-arg IDF_VER=$IDF_VER \
