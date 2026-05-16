e103828d42e365b3fefae9650cbee680a7cccdc0
kishima <kishima7@gmail.com>
2026-05-16 13:24:16 +0900

    set version 0.2.0

--- components/fmrb_common/include/fmrb.h
+++ components/fmrb_common/include/fmrb.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define FMRB_OS_VERSION "0.1.0"
-#define FMRB_GA_VERSION "0.1.0"
-#define FMRB_LINK_VERSION 1
+#define FMRB_OS_VERSION "0.2.0"
+#define FMRB_GA_VERSION "0.2.0"
+#define FMRB_LINK_VERSION 2
