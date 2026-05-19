541508eb038b5b320f0a271589608d6f03564401
kishima <kishima7@gmail.com>
2026-05-19 09:28:16 +0900

    chore(version): bump to 1.0.0 and LINK_VERSION 3

--- components/fmrb_common/include/fmrb.h
+++ components/fmrb_common/include/fmrb.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define FMRB_OS_VERSION "0.2.0"
-#define FMRB_GA_VERSION "0.2.0"
-#define FMRB_LINK_VERSION 2
+#define FMRB_OS_VERSION "1.0.0"
+#define FMRB_GA_VERSION "1.0.0"
+#define FMRB_LINK_VERSION 3
